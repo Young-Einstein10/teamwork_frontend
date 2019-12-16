06a24a54ca9b9a869a88487c2f1c0c2cf8c26898
yusuf abdulrahman <aabdulrahmanyusuf@gmail.com>
2019-12-17 00:50:33 +0100

    Updates

---
+++ precache-manifest.188e7efc06b52785801e47e6d9e9395c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030e2b7b93c7d1fea55118041cece45e",
+    "url": "/teamwork_frontend/index.html"
+  },
+  {
+    "revision": "e45ed083f5c4542c8aa0",
+    "url": "/teamwork_frontend/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e45ed083f5c4542c8aa0",
+    "url": "/teamwork_frontend/static/js/2.1d6e1502.chunk.js"
+  },
+  {
+    "revision": "63e5241ad7feaf4abfb6",
+    "url": "/teamwork_frontend/static/js/main.61a03b05.chunk.js"
+  },
+  {
+    "revision": "187296130f1d1a64247f",
+    "url": "/teamwork_frontend/static/js/runtime-main.70164ea3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/teamwork_frontend/static/media/logo.25bf045c.svg"
+  }
+]);
