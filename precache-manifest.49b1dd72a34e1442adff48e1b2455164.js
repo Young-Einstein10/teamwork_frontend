f80d84f9296afa0393baf5483541fce2b78641eb
yusuf abdulrahman <aabdulrahmanyusuf@gmail.com>
2019-12-30 23:32:13 +0100

    Updates

---
+++ precache-manifest.49b1dd72a34e1442adff48e1b2455164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b5f09415c5979d9cd9b6f0956d961c",
+    "url": "/teamwork_frontend/index.html"
+  },
+  {
+    "revision": "f87c5b33fafad65784a5",
+    "url": "/teamwork_frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fafd97083157e1a844d8",
+    "url": "/teamwork_frontend/static/js/2.ebfd627a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/teamwork_frontend/static/js/2.ebfd627a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f87c5b33fafad65784a5",
+    "url": "/teamwork_frontend/static/js/main.fd6cca0b.chunk.js"
+  },
+  {
+    "revision": "bcb72c0dce83eef1b007",
+    "url": "/teamwork_frontend/static/js/runtime-main.e4be0d88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teamwork_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teamwork_frontend/precache-manifest.ef70307e23498a7aa3c21dd1d1df87a6.js"
+  "/teamwork_frontend/precache-manifest.49b1dd72a34e1442adff48e1b2455164.js"
 );
 
 self.addEventListener('message', (event) => {
